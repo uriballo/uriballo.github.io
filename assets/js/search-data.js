5c9b1778bc2a676bea21106ac4e00775af0211af
uriballo <56364336+uriballo@users.noreply.github.com>
2025-07-07 17:30:40 +0000

    Deploying to gh-pages from @ uriballo/uriballo.github.io@30b6c52355e69f28390174aa7cc70b698f612e9a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -442,14 +442,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%75%72%69%62%61%67%69@%70%72%6F%74%6F%6E%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -463,14 +456,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=6Qa8e4YAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
